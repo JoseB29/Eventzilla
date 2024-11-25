eb4917950b475b30f4871784aadc6edea084463a
Tania Patino-Robledo <tpati4@uic.edu>
2024-11-25 00:02:12 -0600

    Complete Set of Questions

--- Survey.py
+++ Survey.py
@@ -5,7 +5,7 @@ class QuestionnaireApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Event Preferences Questionnaire")
-        self.geometry("400x300")
+        self.geometry("390x844") #Screen Size For An Iphone
         self.current_question = 0
         self.answers = {}
         self.create_widgets()
@@ -16,14 +16,21 @@ class QuestionnaireApp(tk.Tk):
         self.show_question()
 
     def show_question(self):
+        # Clear the container for new content
         for widget in self.container.winfo_children():
             widget.destroy()
 
+        # Navigate to the appropriate question
         if self.current_question == 0:
             self.zip_code_question()
         elif self.current_question == 1:
             self.event_type_question()
-        # Add more elif blocks here for additional questions
+        elif self.current_question == 2:
+            self.music_genre_question()
+        elif self.current_question == 3:
+            self.sports_interest_question()
+        elif self.current_question == 4:
+            self.notification_preferences_question()
         else:
             self.finish()
 
@@ -71,11 +78,107 @@ class QuestionnaireApp(tk.Tk):
 
     def save_event_types(self):
         selected_events = [event for event, var in self.event_vars.items() if var.get() == 1]
-        if not selected_events:
-            messagebox.showwarning("Selection Required", "Please select at least one event type or click Skip.")
-        else:
-            self.answers['event_types'] = selected_events
-            self.next_question()
+        self.answers['event_types'] = selected_events
+        self.next_question()
+
+    def music_genre_question(self):
+        tk.Label(self.container, text="What music genres are you interested in?").pack(pady=10)
+
+        self.music_vars = {
+            "Blues/Gospel": tk.IntVar(),
+            "Classical": tk.IntVar(),
+            "Country": tk.IntVar(),
+            "Electronic/Techno": tk.IntVar(),
+            "House": tk.IntVar(),
+            "Hip Hop/Rap": tk.IntVar(),
+            "Indie": tk.IntVar(),
+            "Jazz": tk.IntVar(),
+            "Latin": tk.IntVar(),
+            "Reggae": tk.IntVar(),
+            "Metal": tk.IntVar(),
+            "Pop": tk.IntVar(),
+            "Rock and Roll": tk.IntVar(),
+            "R&B/Soul": tk.IntVar(),
+            "Opera": tk.IntVar(),
+            "Vocal Music": tk.IntVar()
+        }
+
+        for genre, var in self.music_vars.items():
+            tk.Checkbutton(self.container, text=genre, variable=var).pack(anchor='w')
+
+        button_frame = tk.Frame(self.container)
+        button_frame.pack(pady=20)
+
+        tk.Button(button_frame, text="Skip", command=self.next_question).pack(side="left", padx=10)
+        tk.Button(button_frame, text="Next", command=self.save_music_genres).pack(side="left", padx=10)
+
+    def save_music_genres(self):
+        selected_genres = [genre for genre, var in self.music_vars.items() if var.get() == 1]
+        self.answers['music_genres'] = selected_genres
+        self.next_question()
+
+    def sports_interest_question(self):
+        tk.Label(self.container, text="What sports are you interested in?").pack(pady=10)
+
+        self.sports_vars = {
+            "Soccer": tk.IntVar(),
+            "Football": tk.IntVar(),
+            "Baseball": tk.IntVar(),
+            "Basketball": tk.IntVar(),
+            "Tennis": tk.IntVar(),
+            "Golf": tk.IntVar(),
+            "Hockey": tk.IntVar(),
+            "Cycling": tk.IntVar(),
+            "Track and Field": tk.IntVar(),
+            "Cricket": tk.IntVar(),
+            "Wrestling": tk.IntVar(),
+            "Martial Arts": tk.IntVar(),
+            "Swimming": tk.IntVar(),
+            "Volleyball": tk.IntVar(),
+            "Esports": tk.IntVar()
+        }
+
+        for sport, var in self.sports_vars.items():
+            tk.Checkbutton(self.container, text=sport, variable=var).pack(anchor='w')
+
+        button_frame = tk.Frame(self.container)
+        button_frame.pack(pady=20)
+
+        tk.Button(button_frame, text="Skip", command=self.next_question).pack(side="left", padx=10)
+        tk.Button(button_frame, text="Next", command=self.save_sports_interests).pack(side="left", padx=10)
+
+    def save_sports_interests(self):
+        selected_sports = [sport for sport, var in self.sports_vars.items() if var.get() == 1]
+        self.answers['sports_interests'] = selected_sports
+        self.next_question()
+
+    def notification_preferences_question(self):
+        tk.Label(self.container, text="Set your notification preferences:").pack(pady=10)
+
+        self.price_tracking_var = tk.IntVar()
+        tk.Checkbutton(self.container, text="Enable price tracking notifications", variable=self.price_tracking_var).pack(anchor='w')
+
+        tk.Label(self.container, text="Frequency of Basic Notifications:").pack(pady=5)
+        self.notification_frequency = tk.StringVar(value="Weekly")
+        for frequency in ["Once a Month", "Once a Week", "Daily"]:
+            tk.Radiobutton(self.container, text=frequency, variable=self.notification_frequency, value=frequency).pack(anchor='w')
+
+        tk.Label(self.container, text="Reminders for favorited events:").pack(pady=5)
+        self.reminder_frequency = tk.StringVar(value="Weekly")
+        for reminder in ["Once a Month", "Once a Week", "Daily"]:
+            tk.Radiobutton(self.container, text=reminder, variable=self.reminder_frequency, value=reminder).pack(anchor='w')
+
+        button_frame = tk.Frame(self.container)
+        button_frame.pack(pady=20)
+
+        tk.Button(button_frame, text="Skip", command=self.next_question).pack(side="left", padx=10)
+        tk.Button(button_frame, text="Finish", command=self.save_notifications).pack(side="left", padx=10)
+
+    def save_notifications(self):
+        self.answers['price_tracking'] = bool(self.price_tracking_var.get())
+        self.answers['notification_frequency'] = self.notification_frequency.get()
+        self.answers['reminder_frequency'] = self.reminder_frequency.get()
+        self.next_question()
 
     def next_question(self):
         self.current_question += 1
@@ -88,7 +191,6 @@ class QuestionnaireApp(tk.Tk):
         tk.Label(self.container, text="Thank you for completing the questionnaire!").pack(pady=20)
         tk.Button(self.container, text="Exit", command=self.quit).pack(pady=10)
         print("Collected Answers:", self.answers)
-        # You can add code here to process the collected answers further
 
 if __name__ == "__main__":
     app = QuestionnaireApp()
